de1a23b0f939d8d58bd05ed8012c144df36f01dc
张金龙 <zhangjinl@weipaitang.com>
2022-03-03 19:11:07 +0800

    redis-cluster新增

---
+++ redisCluster/conf.sh
@@ -0,0 +1,5 @@
+for port in `seq 7001 7006`; do \
+  mkdir -p ./redis-cluster/${port}/conf \
+  && PORT=${port} envsubst < ./redis-cluster.tmpl > ./redis-cluster/${port}/conf/redis.conf \
+  && mkdir -p ./redis-cluster/${port}/data; \
+done
